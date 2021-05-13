a836795e0c39e40e4c8196c1423e546a0f0b6e7e
Sabiha Uysal <sabihauysal93@outlook.com>
2021-05-13 18:29:10 +0300

    Add project files.

---
+++ FrontendMaraton/JavaScript.js
@@ -0,0 +1,34 @@
+﻿function hover(element) {
+    element.setAttribute('src', '../../Resimler/1.png');
+}
+
+function unhover(element) {
+    element.setAttribute('src', '../../Resimler/2.png');
+}
+function hover2(element) {
+    element.setAttribute('src', '../../Resimler/2.png');
+}
+
+function unhover2(element) {
+    element.setAttribute('src', '../../Resimler/3.png');
+}
+
+var buton = document.getElementById('yakalanacakNesne');
+buton.onmouseover = function () {
+    var xPos = parseInt(Math.random() * innerWidth/2);
+    var yPos = parseInt(Math.random() * innerHeight/2);
+   
+    buton.style.left = xPos + "px";
+    buton.style.top = yPos + "px";
+
+}
+
+buton.onclick = function () {
+    alert("Tebrikler Kazandın! Ücretsiz giriş için kodunuz:SA9396");
+}
+
+function freeTicketInformation() {
+    setTimeout(function () {
+        alert("Hoşgeldin! Cluba bedava giriş hakkı kazanmak istersen sol üstte bulunan mavi topu yakala! ");
+    }, 1000);
+}
